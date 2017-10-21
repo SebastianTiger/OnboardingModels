82434688e22ef683b620092623a9646e0d065ab7
afpatmin <patrick.minogue@appfoundry.se>
2017-10-21 13:55:43 +0200

    updated angular_components dependency to 0.8.0

--- lib/src/model/action.dart
+++ lib/src/model/action.dart
@@ -3,7 +3,10 @@ part of model_base;
 class Action extends ModelBase
 {
   @override
-  Action() : super();
+  Action() : super()
+  {
+    pushNotificationRegistry = new List();
+  }
 
   @override
   Action.decode(Map<String, dynamic> data) : super.decode(data)
@@ -12,12 +15,15 @@ class Action extends ModelBase
     description = data["description"];
     location = data["location"];
     learningContentId = data["learning_content_id"];
+    pushNotificationRegistry = (data.containsKey("push_notification_registry")) ? JSON.decode(data["push_notification_registry"]) : new List();
   }
 
   @override
   Map<String, dynamic> encode()
   {
     Map<String, dynamic> data = super.encode();
+    data["push_notification_registry"] = JSON.encode(pushNotificationRegistry);
+
     return data;
   }
 
@@ -36,6 +42,7 @@ class Action extends ModelBase
     description = other.description;
     location = other.location;
     learningContentId = other.learningContentId;
+    pushNotificationRegistry = other.pushNotificationRegistry == null ? new List() : new List.from(other.pushNotificationRegistry);
   }
 
   Map<String, dynamic> get courseIndexEncoded => {"id":id, "day":day, "time":time, "highlight":highlight};
@@ -45,16 +52,17 @@ class Action extends ModelBase
   String get description => _properties["description"];
   String get location => _properties["location"];
   String get learningContentId => _properties["learning_content_id"];
+  List<Map<String, dynamic>> get pushNotificationRegistry => _properties["push_notification_registry"];
 
   void set name(String value) { _properties["name"] = value; }
   void set description(String value) { _properties["description"] = value; }
   void set location(String value) { _properties["location"] = value; }
   void set learningContentId(String value) { _properties["learning_content_id"] = value; }
+  void set pushNotificationRegistry(List<Map<String, dynamic>> value) { _properties["push_notification_registry"] = value; }
 
   int day = 0;
   DateTime date;
   String time = "12:00";
   bool highlight = false;
-
   bool popupOpen = false;
 }
